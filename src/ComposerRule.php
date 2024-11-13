36e03e1fc08f51fb483aaaa293069cac9383b696
pb30 <pb30@pb30.com>
2024-11-13 15:26:31 -0600

    Fix types

--- src/ComposerRule.php
+++ src/ComposerRule.php
@@ -6,6 +6,7 @@ use PhpParser\Node;
 use PHPStan\Analyser\Scope;
 use PHPStan\Node\CollectedDataNode;
 use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleError;
 
 class ComposerRule implements Rule
 {
@@ -21,6 +22,9 @@ class ComposerRule implements Rule
         return CollectedDataNode::class;
     }
 
+    /**
+     * @return RuleError[]
+     */
     public function processNode(Node $node, Scope $scope): array
     {
         $errors = $this->depAnalyzer::$results;
